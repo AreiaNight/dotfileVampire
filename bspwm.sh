5295d88f28219806e25c7b1579ca6550fbff23e0
ThePunkster <115718752+ThePunkster@users.noreply.github.com>
2024-01-03 19:09:43 -0500

    Update bspwm.sh
    full

--- bspwm.sh
+++ bspwm.sh
@@ -4,6 +4,22 @@
 sudo apt update
 sudo apt install -y xorg bspwm sxhkd rxvt-unicode feh compton
 
+# Instalando dependencias de Entorno
+
+sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+
+#Instalando Requerimientos para la polybar
+
+sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
+
+# Dependencias de Picom
+
+sudo apt install -y meson picom libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
+
+# Paquetes adicionales
+sudo apt install -y feh  rofi xclip  locate ranger neofetch wmname imagemagick cmatrix
+
+
 # Configurar el gestor de ventanas BSPWM y SXHKD
 mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/sxhkd
@@ -25,11 +41,6 @@ vsync = \"opengl-swc\";" >> ~/.config/compton.conf
 echo "exec bspwm" > ~/.xinitrc
 
 echo "Instalación completa. Por favor, reinicia tu sistema o inicia una nueva sesión para utilizar BSPWM."
-
-
-# Actualizar el sistema e instalar dependencias
-sudo apt update
-sudo apt install -y xorg bspwm sxhkd rxvt-unicode feh compton
 
 # Instalar kitty o actualizar a la última versión
 if command -v kitty &>/dev/null; then
@@ -72,3 +83,36 @@ chmod +x ~/.config/bspwm/bspwmrc
 echo "exec bspwm" > ~/.xinitrc
 
 echo "Instalación completa. Por favor, reinicia tu sistema o inicia una nueva sesión para utilizar BSPWM con kitty."
+
+# Instalando Polybar
+
+cd ~/github/polybar
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
+
+# Instalando Picom
+
+cd ~/github/picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
+# Instalando p10k
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Instalando p10k root
+
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
+
+# Plugins ZSH
+
+sudo apt install -y zsh-syntax-highlighting zsh-autosuggestions
+sudo mkdir /usr/share/zsh-sudo
+cd /usr/share/zsh-sudo
+sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
